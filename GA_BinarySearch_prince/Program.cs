6d850e637d1e664294fce3fbd35b7acffd4d1fae
princej24 <116045993+princej24@users.noreply.github.com>
2024-03-06 15:13:02 -0800

    Add project files.

---
+++ GA_BinarySearch_prince/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_BinarySearch_prince
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
